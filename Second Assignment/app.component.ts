57dfbc24dfc600ef949f58862923ef35fc194fdc
SachiN <SachiN>
2018-06-23 02:41:11 +0530

    Assignments

---
+++ Second Assignment/app.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1 align="center">Marvellous infosystem</h1>
+  <input type="text"  placeholder="write something" id="one"/>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
